f8ccba2a10cbcd3976737e0d2b4a3a47261eb099
RubenOlander <rubenolander@gmail.com>
2024-09-03 11:15:01 +0200

    Changed two files.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,6 +3,9 @@ export default function Contact() {
     <main className="px-8">
       <h1>Kanske ett kontaktformulär.</h1>
       <p>Paragraf för testing.</p>
+      <p>Paragraf för rebase.</p>
+      <p>Paragraf för rebase.</p>
+      <p>Paragraf för rebase.</p>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="px-8">
       <h1>Startsida</h1>
+      <p>Test för rebase. Rebase.</p>
     </main>
   );
 }
